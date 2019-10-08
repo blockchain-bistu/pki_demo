8eca2688dc905d9c1d50d05dfc4086648506c61f
franklin-zf <m18513290580@163.com>
2019-10-08 11:56:45 +0800

    first commit

---
+++ pki/sm2.py
@@ -0,0 +1,412 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Franklin
+@file: sm2.py
+@time: 2019-10-06 20:18
+@dec: 
+"""
+ 
+
+
+from functools import reduce
+from random import choices, randint
+from pki.sm3 import KDF
+from pki import hashlib
+from collections import namedtuple
+# from astartool.random import random_hex_string
+# 选择素域，设置椭圆曲线参数
+sm2_N = int('FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123', 16)
+sm2_P = int('FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF', 16)
+sm2_G = '32c4ae2c1f1981195f9904466a39c9948fe30bbff2660be1715a4589334c74c7bc3736a2f4f6779c59bdcee36b692153d0a9877cc62a474002df32e52139f0a0'  # G点
+sm2_G_number = int(sm2_G, 16)
+sm2_a = int('FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFC', 16)
+sm2_b = int('28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93', 16)
+sm2_a_3 = (sm2_a + 3) % sm2_P  # 倍点用到的中间值
+Fp = 256
+
+def random_hex_string(n: int = 32, upper=False):
+    """
+    生成随机16进制数
+    :param n:
+    :param upper:
+    :return:
+    """
+    hex_string = '0123456789abcdef'
+    hex_string_upper = '0123456789ABCDFEF'
+    return ''.join(choices(hex_string_upper, k=n)) if upper else ''.join(choices(hex_string, k=n))
+
+
+# sm2_N = int('BDB6F4FE3E8B1D9E0DA8C0D40FC962195DFAE76F56564677', 16)
+# sm2_P = int('BDB6F4FE3E8B1D9E0DA8C0D46F4C318CEFE4AFE3B6B8551F', 16)
+# sm2_G = '4AD5F7048DE709AD51236DE65E4D4B482C836DC6E410664002BB3A02D4AAADACAE24817A4CA3A1B014B5270432DB27D2'# G点
+# sm2_a = int('BB8E5E8FBC115E139FE6A814FE48AAA6F0ADA1AA5DF91985',16)
+# sm2_b = int('1854BEBDC31B21B7AEFC80AB0ECD10D5B1B3308E6DBF11C1',16)
+# sm2_a_3 = (sm2_a + 3) % sm2_P # 倍点用到的中间值
+# Fp = 192
+
+
+def modular_power(a, n, p):
+    """
+    计算a^ n % p
+    if n == 0:
+        return 1
+    elif n == 1:
+        return a % p
+    temp = a * a % p
+    if n & 1:
+        return a % p * modular_power(temp, n // 2, p) % p
+    else:
+        return (modular_power(temp, n // 2, p)) % p
+    原文：https://blog.csdn.net/qq_36921652/article/details/79368299
+    """
+    return pow(a, n, p)
+
+
+def is_prime(number: (str, int), itor=10):
+    if not isinstance(number, int):
+        number = int(number)
+    for i in range(itor):
+        a = randint(1, number - 1)
+        if modular_power(a, number - 1, number) != 1:
+            return False
+    return True
+
+
+def get_hash(algorithm_name, message, Hexstr=0, encoding='utf-8'):
+    if hasattr(hashlib, algorithm_name):
+        f = getattr(hashlib, algorithm_name)()
+    else:
+        raise ValueError('method does not exists')
+    if Hexstr:
+        message = bytes.fromhex(message)
+    if isinstance(message, (bytes, bytearray)):
+        f.update(message)
+    else:
+        f.update(bytes(message, encoding=encoding))
+    return f.hexdigest()
+
+
+def get_random_str(n: int = 64):
+    return random_hex_string(n)
+
+
+def kG(k, Point, len_para):
+    """
+    kP运算
+    :param k:
+    :param Point:
+    :param len_para:
+    :return:
+    """
+    Point += '1'
+    Temp = reduce(
+        lambda x, y: AddPoint(DoublePoint(x, len_para), Point, len_para) if y is '1' else DoublePoint(x, len_para),
+        bin(k)[3:], Point)
+    return ConvertJacb2Nor(Temp, len_para)
+
+
+def DoublePoint(Point, len_para, P=sm2_P):
+    """
+    倍点
+    :param Point:
+    :param len_para:
+    :param P:
+    :return:
+    """
+    length = len(Point)
+    len_2 = 2 * len_para
+    if length < len_2:
+        return None
+    else:
+        x1 = int(Point[0:len_para], 16)
+        y1 = int(Point[len_para:len_2], 16)
+        z1 = 1 if length == len_2 else int(Point[len_2:], 16)
+        T6, T2 = (z1 * z1) % P, (y1 * y1) % P
+        T3, T4 = (x1 + T6) % P,(x1 - T6) % P
+        T1, T3 = (T3 * T4) % P, (y1 * z1) % P
+        T4 = (T2 * 8) % P
+        T5 = (x1 * T4) % P
+        T1, T6 = (T1 * 3) % P, (sm2_a_3 * T6 * T6) % P
+        T1 = (T1 + T6) % P
+        z3, T3 = (T3 + T3) % P, (T1 * T1) % P
+        T2, x3 = (T2 * T4) % P, (T3 - T5) % P
+        T4 = (T5 + ((T5 + P) >> 1) - T3) % P if T5 % 2 else (T5 + (T5 >> 1) - T3) % P
+        T1 = (T1 * T4) % P
+        y3 = (T1 - T2) % P
+
+        form = '%%0%dx' % len_para
+        form = form * 3
+        return form % (x3, y3, z3)
+
+
+def AddPoint(P1, P2, len_para, P=sm2_P):
+    """点加函数
+    :param P1 为Jacobian加重射影坐标
+    :param P2 为仿射坐标即z=1
+    """
+    len_2 = 2 * len_para
+    l1, l2 = len(P1), len(P2)
+    if (l1 < len_2) or (l2 < len_2):
+        return None
+    else:
+        X1, Y1 = int(P1[0:len_para], 16), int(P1[len_para:len_2], 16)
+        Z1 = 1 if l1 == len_2 else int(P1[len_2:], 16)
+        x2, y2 = int(P2[0:len_para], 16), int(P2[len_para:len_2], 16)
+        T1, T2 = (Z1 * Z1) % P, (y2 * Z1) % P
+        T3, T1 = (x2 * T1) % P, (T1 * T2) % P
+        T2, T3 = (T3 - X1) % P, (T3 + X1) % P
+        T4, T1, Z3 = (T2 * T2) % P, (T1 - Y1) % P, (Z1 * T2) % P
+        T2, T3, T5 = (T2 * T4) % P, (T3 * T4) % P, (T1 * T1) % P
+        T4, X3, T2 = (X1 * T4) % P, (T5 - T3) % P, (Y1 * T2) % P
+        T3 = (T4 - X3) % P
+        T1 = (T1 * T3) % P
+        Y3 = (T1 - T2) % P
+
+        form = '%%0%dx' % len_para
+        form = form * 3
+        return form % (X3, Y3, Z3)
+
+
+def ConvertJacb2Nor(Point, len_para, P=sm2_P):
+    """Jacobian加重射影坐标转换成仿射坐标"""
+    len_2 = 2 * len_para
+    x = int(Point[0:len_para], 16)
+    y = int(Point[len_para:len_2], 16)
+    z = int(Point[len_2:], 16)
+    # z_inv = Inverse(z, P)
+    z_inv = pow(z, P - 2, P)
+    z_invSquar = (z_inv * z_inv) % P
+    z_invQube = (z_invSquar * z_inv) % P
+    x_new = (x * z_invSquar) % P
+    y_new = (y * z_invQube) % P
+    z_new = (z * z_inv) % P
+    if z_new == 1:
+        form = '%%0%dx' % len_para
+        form = form * 2
+        return form % (x_new, y_new)
+    else:
+        print("Point at infinity!!!!!!!!!!!!")
+        return None
+
+
+def Inverse(data, M, len_para=64):
+    """ 求逆, 可用pow（）代替"""
+    tempM = M - 2
+    mask_str = '8' + '0' * (len_para - 1)
+    mask = int(mask_str, 16)
+    tempA = 1
+    tempB = data
+
+    for i in range(len_para * 4):
+        tempA = (tempA * tempA) % M
+        if (tempM & mask) != 0:
+            tempA = (tempA * tempB) % M
+        mask = mask >> 1
+
+    return tempA
+
+
+def Verify(Sign, E, PA, len_para=64, Hexstr=0, encoding='utf-8'):
+    """
+    验签函数
+    :param Sign: 签名 r||s
+    :param E: E消息hash
+    :param PA: PA公钥
+    :param len_para:
+    :param Hexstr: 是否是16进制数：1:是 0:不是
+    :param encoding: 编码格式
+    :return:
+    """
+    if isinstance(Sign, str):
+        r = int(Sign[0:len_para], 16)
+        s = int(Sign[len_para:2 * len_para], 16)
+    elif isinstance(Sign, bytes):
+        r = int(Sign.hex()[:len_para], 16)
+        s = int(Sign.hex()[len_para:2 * len_para], 16)
+
+    if Hexstr:
+        e = int(E, 16)  # 输入消息本身是16进制字符串
+    else:
+        if isinstance(E, str):
+            E = E.encode(encoding)
+        E = E.hex()  # 消息转化为16进制字符串
+        e = int(E, 16)
+
+    if isinstance(PA, str):
+        pass
+    elif isinstance(PA, (bytes, bytearray)):
+        PA = PA.hex()
+    else:
+        raise ValueError('Typeof PA must be string or bytes')
+    t = (r + s) % sm2_N
+    if t == 0:
+        return 0
+
+    P1 = kG(s, sm2_G, len_para)
+    P2 = kG(t, PA, len_para)
+    # print(P1)
+    # print(P2)
+    if P1 == P2:
+        P1 += '1'
+        P1 = DoublePoint(P1, len_para)
+    else:
+        P1 += '1'
+        P1 = AddPoint(P1, P2, len_para)
+        P1 = ConvertJacb2Nor(P1, len_para)
+
+    x = int(P1[0:len_para], 16)
+    return r == ((e + x) % sm2_N)
+
+
+def Sign(E, DA, K, len_para, Hexstr=0, encoding='utf-8'):
+    """签名函数
+     :param E 消息的hash, 16进制字符串
+     :param DA私钥, 16进制字符串
+     :param K 随机数, 16进制字符串
+     """
+    if Hexstr:
+        e = int(E, 16)  # 输入消息本身是16进制字符串
+    else:
+        if isinstance(E, str):
+            E = E.encode(encoding)
+        E = E.hex()  # 消息转化为16进制字符串
+        e = int(E, 16)
+    if isinstance(DA, str):
+        d = int(DA, 16)
+    elif isinstance(DA, (bytes, bytearray)):
+        d = int(DA.hex(), 16)
+    else:
+        raise ValueError('DA must be str or bytes')
+    k = int(K, 16)
+
+    P1 = kG(k, sm2_G, len_para)
+
+    x = int(P1[:len_para], 16)
+    R = (e + x) % sm2_N
+    if R == 0 or R + k == sm2_N:
+        return None
+    d_1 = pow(d + 1, sm2_N - 2, sm2_N)
+    S = (d_1 * (k + R) - R) % sm2_N
+    s = '%0{}x%0{}x'.format(len_para, len_para) % (R, S) if S else None
+    return bytes.fromhex(s)
+
+
+def Encrypt(M, PA, len_para, Hexstr=0, encoding='utf-8', hash_algorithm='sm3'):
+    """
+    加密函数
+    :param M: 消息
+    :param PA: PA公钥
+    :param len_para: 目前固定为64
+    :param Hexstr: M是否是hex字符串
+    :param encoding: 若M不是16进制字符串
+    :param hash_algorithm:
+    :return:
+    """
+    if Hexstr:
+        msg = M  # 输入消息本身是16进制字符串
+    else:
+        if isinstance(M, str):
+            msg = M.encode(encoding)
+        else:
+            msg = M
+        msg = msg.hex()  # 消息转化为16进制字符串
+    if isinstance(PA, str):
+        pass
+    elif isinstance(PA, (bytes, bytearray)):
+        PA = PA.hex()
+    else:
+        raise ValueError('Typeof PA must be string or bytes')
+    k = get_random_str(len_para)
+    # k = '59276E27D506861A16680F3AD9C02DCCEF3CC1FA3CDBE4CE6D54B80DEAC1BC21'
+    # k = '384F30353073AEECE7A1654330A96204D37982A3E15B2CB5'
+    C1 = kG(int(k, 16), sm2_G, len_para)
+    # print('C1 = %s'%C1)
+    xy = kG(int(k, 16), PA, len_para)
+    # print('xy = %s' % xy)
+    x2 = xy[0:len_para]
+    y2 = xy[len_para:2 * len_para]
+    ml = len(msg)
+    t = KDF(xy, ml / 2)
+    if int(t, 16) == 0:
+        return None
+    else:
+        form = '%%0%dx' % ml
+        C2 = form % (int(msg, 16) ^ int(t, 16))
+        # print('C2 = %s'% C2)
+        # print('%s%s%s'% (x2,msg,y2))
+        C3 = get_hash(hash_algorithm, '%s%s%s' % (x2, msg, y2), Hexstr=1)
+        # print('C3 = %s' % C3)
+        return bytes.fromhex('%s%s%s' % (C1, C3, C2))
+
+
+def Decrypt(C, DA, len_para, Hexstr=0, encoding='utf-8', hash_algorithm='sm3'):
+    """
+    解密函数，
+    :param C 密文（16进制字符串）
+    :param DA 私钥
+    :param len_para 长度，目前只支持64
+    """
+    f = getattr(hashlib, hash_algorithm)()
+    if isinstance(DA, str):
+        pass
+    elif isinstance(DA, (bytes, bytearray)):
+        DA = DA.hex()
+    else:
+        raise ValueError('DA must be str or bytes')
+    len_2 = 2 * len_para
+    len_3 = len_2 + f.digest_size * 2
+    if not Hexstr:
+        if isinstance(C, bytes):
+            C = C.hex()
+
+    C1 = C[0:len_2]
+    C3 = C[len_2:len_3]
+    C2 = C[len_3:]
+    xy = kG(int(DA, 16), C1, len_para)
+    # print('xy = %s' % xy)
+    x2 = xy[0:len_para]
+    y2 = xy[len_para:len_2]
+    cl = len(C2)
+    # print(cl)
+    t = KDF(xy, cl / 2)
+    if int(t, 16) == 0:
+        return None
+    else:
+        form = '%%0%dx' % cl
+        M = form % (int(C2, 16) ^ int(t, 16))
+        # print('M = %s' % M)
+
+        u = get_hash(hash_algorithm, '%s%s%s' % (x2, M, y2), 1)
+        return bytes.fromhex(M) if u == C3 else None
+
+
+KeyPair = namedtuple('KeyPair', ['publicKey', 'privateKey'])
+
+
+def generate_keypair(len_param=64):
+    d = get_random_str(len_param)
+    PA = kG(int(d, 16), sm2_G, len_param)
+    return KeyPair(bytes.fromhex(PA), bytes.fromhex(d))
+
+
+
+if __name__ == '__main__':
+    # print(generate_keypair(64))
+    pk, sk = generate_keypair()
+    print(sk.hex())
+    print(pk.hex())
+    len_para = 64
+
+    sig = Sign("你好", sk, '12345678abcdef', len_para)
+    print(sig.hex())
+    resSig = Verify(sig, "你好", pk, len_para)
+    print(resSig,type(resSig))
+
+    e = b'hello'
+
+    C = Encrypt(e, pk, len_para, 0)
+    print(C.hex())
+    m = Decrypt(C, sk, len_para)
+
+    print(m.hex())
---
+++ pki/sm3.py
@@ -0,0 +1,305 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Franklin
+@file: sm3.py
+@time: 2019-10-06 20:18
+@dec: 
+"""
+ 
+from math import ceil
+from functools import reduce
+from copy import deepcopy, copy
+import struct
+
+BIT_BLOCK_H = [0x00, 0x80, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC, 0xFE, 0xFF]
+BIT_BLOCK_L = [0x0, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF]
+BIT_EACH = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+BIT_EACH_32 = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144,
+               524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456,
+               536870912, 1073741824, 2147483648, 4294967296]
+IV = "7380166f 4914b2b9 172442d7 da8a0600 a96f30bc 163138aa e38dee4d b0fb0e4e"
+IV = int(IV.replace(" ", ""), 16)
+IV = [(IV >> ((7 - i) * 32)) & 0xFFFFFFFF for i in range(8)]
+
+
+def rotate_left(a, k):
+    k %= 32
+    high, low = divmod(a, BIT_EACH_32[32 - k])
+    return high + low * BIT_EACH_32[k]
+
+
+T_j = [0x79cc4519] * 16 + [0x7a879d8a] * 48
+T_j_rotate_left = [rotate_left(Tj, j) for j, Tj in enumerate(T_j)]
+
+
+def FF_j(X, Y, Z, j):
+    # 已经融合到内部了
+    return X ^ Y ^ Z if 0 <= j < 16 else (X & Y) | (X & Z) | (Y & Z)
+
+
+def GG_j(X, Y, Z, j):
+    # 已经融合在内部了
+    return X ^ Y ^ Z if 0 <= j < 16 else (X & Y) | ((~ X) & Z)
+
+
+def P_0(X):
+    high, low = divmod(X, BIT_EACH_32[23])
+    r_l_9 = high + low * BIT_EACH_32[9]
+    high, low = divmod(X, BIT_EACH_32[15])
+    r_l_17 = high + low * BIT_EACH_32[17]
+    # return X ^ (rotate_left(X, 9)) ^ (rotate_left(X, 17))
+    return X ^ r_l_9 ^ r_l_17
+
+
+def P_1(X):
+    high, low = divmod(X, BIT_EACH_32[17])
+    r_l_15 = high + low * BIT_EACH_32[15]
+    high, low = divmod(X, BIT_EACH_32[9])
+    r_l_23 = high + low * BIT_EACH_32[23]
+    return X ^ r_l_15 ^ r_l_23
+
+
+def PUT_UINT32_BE(n):
+    return [int((n >> 24) & 0xff), int((n >> 16) & 0xff), int((n >> 8) & 0xff), int(n & 0xff)]
+
+
+def __cf_reduce_16_64(V_i, j, W=None):
+    high_A12, low_A12 = divmod(V_i[0], BIT_EACH_32[20])
+    r_l_12 = high_A12 + low_A12 * BIT_EACH_32[12]
+    high, low = divmod((r_l_12 + V_i[4] + T_j_rotate_left[j]) & 0xFFFFFFFF, BIT_EACH_32[25])
+    SS1 = high + low * BIT_EACH_32[7]
+    SS2 = SS1 ^ r_l_12
+    # FF
+    TT1 = (((V_i[0] & V_i[1]) | (V_i[0] & V_i[2]) | (V_i[1] & V_i[2])) + V_i[3] + SS2 + (W[j] ^ W[j + 4])) & 0xFFFFFFFF
+    # GG
+    TT2 = (((V_i[4] & V_i[5]) | ((~ V_i[4]) & V_i[6])) + V_i[7] + SS1 + W[j]) & 0xFFFFFFFF
+    high_B9, low_B9 = divmod(V_i[1], BIT_EACH_32[23])
+    high_F19, low_F19 = divmod(V_i[5], BIT_EACH_32[13])
+    return [TT1, V_i[0], high_B9 + low_B9 * BIT_EACH_32[9] & 0xffffffff, V_i[2], P_0(TT2) & 0xffffffff, \
+            V_i[4], high_F19 + low_F19 * BIT_EACH_32[19] & 0xffffffff, V_i[6]]
+
+
+def __cf_reduce_0_16(V_i, j, W=None):
+    high_A12, low_A12 = divmod(V_i[0], BIT_EACH_32[20])
+    r_l_12 = high_A12 + low_A12 * BIT_EACH_32[12]
+    high, low = divmod((r_l_12 + V_i[4] + T_j_rotate_left[j]) & 0xFFFFFFFF, BIT_EACH_32[25])
+    SS1 = high + low * BIT_EACH_32[7]
+    SS2 = SS1 ^ r_l_12
+    # FF
+    TT1 = ((V_i[0] ^ V_i[1] ^ V_i[2]) + V_i[3] + SS2 + (W[j] ^ W[j + 4])) & 0xFFFFFFFF
+    # GG
+    TT2 = ((V_i[4] ^ V_i[5] ^ V_i[6]) + V_i[7] + SS1 + W[j]) & 0xFFFFFFFF
+    high_B9, low_B9 = divmod(V_i[1], BIT_EACH_32[23])
+    high_F19, low_F19 = divmod(V_i[5], BIT_EACH_32[13])
+    high, low = divmod(TT2, BIT_EACH_32[23])
+    r_l_9 = high + low * BIT_EACH_32[9]
+    high, low = divmod(TT2, BIT_EACH_32[15])
+    r_l_17 = high + low * BIT_EACH_32[17]
+    return [TT1, V_i[0], high_B9 + low_B9 * BIT_EACH_32[9] & 0xffffffff, V_i[2], (TT2 ^ r_l_9 ^ r_l_17) & 0xffffffff, \
+            V_i[4], high_F19 + low_F19 * BIT_EACH_32[19] & 0xffffffff, V_i[6]]
+
+
+def CF(V_i, B_i):
+    W = [(B_i[ind] * BIT_EACH_32[24]) + (B_i[ind + 1] * BIT_EACH_32[16]) + (B_i[ind + 2] * BIT_EACH_32[8]) + (
+        B_i[ind + 3]) for ind in range(0, 64, 4)]
+    for j in range(16, 68):
+        high_W3_15, low_W3_15 = divmod(W[-3], BIT_EACH_32[17])
+        high_W13_7, low_W13_7 = divmod(W[-13], BIT_EACH_32[25])
+        # P_1
+        X = W[- 16] ^ W[- 9] ^ (high_W3_15 + low_W3_15 * BIT_EACH_32[15])
+        high_P1_15, low_P1_15 = divmod(X, BIT_EACH_32[17])
+        r_l_15 = high_P1_15 + low_P1_15 * BIT_EACH_32[15]
+        high_P1_23, low_P1_23 = divmod(X, BIT_EACH_32[9])
+        r_l_23 = high_P1_23 + low_P1_23 * BIT_EACH_32[23]
+        # return X ^ (rotate_left(X, 15)) ^ (rotate_left(X, 23))
+        W.append(X ^ r_l_15 ^ r_l_23 ^ (high_W13_7 + low_W13_7 * BIT_EACH_32[7]) ^ W[- 6])
+        # W.append(P_1(W[- 16] ^ W[- 9] ^ (high_W3_15 + low_W3_15 * BIT_EACH_32[15])) ^ (
+        #         high_W13_7 + low_W13_7 * BIT_EACH_32[7]) ^ W[- 6])
+    W_1 = [W[j] ^ W[j + 4] for j in range(64)]
+    A, B, C, D, E, F, G, H = V_i
+    for j in range(0, 16):
+        high_A12, low_A12 = divmod(A, BIT_EACH_32[20])
+        r_l_12 = high_A12 + low_A12 * BIT_EACH_32[12]
+        high, low = divmod((r_l_12 + E + T_j_rotate_left[j]) & 0xFFFFFFFF, BIT_EACH_32[25])
+        SS1 = high + low * BIT_EACH_32[7]
+        SS2 = SS1 ^ r_l_12
+        # Wj = (B_i[ind] * BIT_EACH_32[24]) + (B_i[ind + 1] * BIT_EACH_32[16]) + (B_i[ind + 2] * BIT_EACH_32[8]) + (B_i[ind + 3])
+        # FF
+        TT1 = ((A ^ B ^ C) + D + SS2 + W_1[j]) & 0xFFFFFFFF
+        # GG
+        TT2 = ((E ^ F ^ G) + H + SS1 + W[j]) & 0xFFFFFFFF
+        high_B9, low_B9 = divmod(B, BIT_EACH_32[23])
+        high_F19, low_F19 = divmod(F, BIT_EACH_32[13])
+        high, low = divmod(TT2, BIT_EACH_32[23])
+        r_l_9 = high + low * BIT_EACH_32[9]
+        high, low = divmod(TT2, BIT_EACH_32[15])
+        r_l_17 = high + low * BIT_EACH_32[17]
+        A, B, C, D, E, F, G, H = TT1, A, high_B9 + low_B9 * BIT_EACH_32[9] & 0xffffffff, C, (
+                TT2 ^ r_l_9 ^ r_l_17) & 0xffffffff, E, high_F19 + low_F19 * BIT_EACH_32[19] & 0xffffffff, G
+    for j in range(16, 64):
+        high_A12, low_A12 = divmod(A, BIT_EACH_32[20])
+        r_l_12 = high_A12 + low_A12 * BIT_EACH_32[12]
+        high, low = divmod((r_l_12 + E + T_j_rotate_left[j]) & 0xFFFFFFFF, BIT_EACH_32[25])
+        SS1 = high + low * BIT_EACH_32[7]
+        SS2 = SS1 ^ r_l_12
+        # FF
+        TT1 = (((A & B) | (A & C) | (B & C)) + D + SS2 + W_1[j]) & 0xFFFFFFFF
+        # GG
+        TT2 = (((E & F) | ((~ E) & G)) + H + SS1 + W[j]) & 0xFFFFFFFF
+        high_B9, low_B9 = divmod(B, BIT_EACH_32[23])
+        high_F19, low_F19 = divmod(F, BIT_EACH_32[13])
+        high, low = divmod(TT2, BIT_EACH_32[23])
+        r_l_9 = high + low * BIT_EACH_32[9]
+        high, low = divmod(TT2, BIT_EACH_32[15])
+        r_l_17 = high + low * BIT_EACH_32[17]
+        A, B, C, D, E, F, G, H = TT1, A, high_B9 + low_B9 * BIT_EACH_32[9] & 0xffffffff, C, (
+                TT2 ^ r_l_9 ^ r_l_17) & 0xffffffff, E, high_F19 + low_F19 * BIT_EACH_32[19] & 0xffffffff, G
+    return [A ^ V_i[0], B ^ V_i[1], C ^ V_i[2],
+            D ^ V_i[3], E ^ V_i[4], F ^ V_i[5], G ^ V_i[6], H ^ V_i[7]]
+
+
+def CF2(V_i, B_i):
+    W = [(B_i[ind] * BIT_EACH_32[24]) + (B_i[ind + 1] * BIT_EACH_32[16]) + (B_i[ind + 2] * BIT_EACH_32[8]) + (
+        B_i[ind + 3]) for ind in range(0, 64, 4)]
+    for j in range(16, 68):
+        high_W3_15, low_W3_15 = divmod(W[-3], BIT_EACH_32[17])
+        high_W13_7, low_W13_7 = divmod(W[-13], BIT_EACH_32[25])
+        W.append(P_1(W[- 16] ^ W[- 9] ^ (high_W3_15 + low_W3_15 * BIT_EACH_32[15])) ^ (
+                high_W13_7 + low_W13_7 * BIT_EACH_32[7]) ^ W[- 6])
+    V_i_1 = reduce(lambda a, b: __cf_reduce_0_16(a, b, W), range(0, 16), V_i.copy())
+    V_i_2 = reduce(lambda a, b: __cf_reduce_16_64(a, b, W), range(16, 64), V_i_1)
+    return [V_i_2[0] ^ V_i[0], V_i_2[1] ^ V_i[1], V_i_2[2] ^ V_i[2],
+            V_i_2[3] ^ V_i[3], V_i_2[4] ^ V_i[4], V_i_2[5] ^ V_i[5], V_i_2[6] ^ V_i[6], V_i_2[7] ^ V_i[7]]
+
+
+def CF3(V_i, B_i):
+    W = [(B_i[ind] * BIT_EACH_32[24]) + (B_i[ind + 1] * BIT_EACH_32[16]) + (B_i[ind + 2] * BIT_EACH_32[8]) + (
+        B_i[ind + 3]) for ind in range(0, 64, 4)]
+    for j in range(16, 68):
+        # a = (P_1(W[- 16] ^ W[- 9] ^ (rotate_left(W[- 3], 15))) ^ (rotate_left(W[- 13], 7)) ^ W[- 6])
+        high_W3_15, low_W3_15 = divmod(W[-3], BIT_EACH_32[17])
+        high_W13_7, low_W13_7 = divmod(W[-13], BIT_EACH_32[25])
+        W.append(P_1(W[- 16] ^ W[- 9] ^ (high_W3_15 + low_W3_15 * BIT_EACH_32[15])) ^ (
+                high_W13_7 + low_W13_7 * BIT_EACH_32[7]) ^ W[- 6])
+    V_i_1 = reduce(lambda a, b: __cf_reduce_0_16(a, b, W), range(0, 16), V_i.copy())
+    V_i_2 = reduce(lambda a, b: __cf_reduce_16_64(a, b, W), range(16, 64), V_i_1)
+    return [V_i_2[0] ^ V_i[0], V_i_2[1] ^ V_i[1], V_i_2[2] ^ V_i[2],
+            V_i_2[3] ^ V_i[3], V_i_2[4] ^ V_i[4], V_i_2[5] ^ V_i[5], V_i_2[6] ^ V_i[6], V_i_2[7] ^ V_i[7]]
+
+
+def digest(msg, Hexstr=0):
+    if isinstance(msg, list):
+        pass
+    else:
+        msg = hex2byte(msg) if Hexstr else str2bytes(msg)
+    len1 = len(msg)
+    msg.append(0x80)
+    reserve1 = len1 % 64 + 1
+    range_end = 56 if reserve1 <= 56 else 120
+    msg.extend([0] * (range_end - reserve1))
+    bit_length = len1 * 8
+    msg.extend(struct.pack(">Q", bit_length))
+    B = (msg[i:i + 64] for i in range(0, len(msg), 64))
+    y = reduce(CF, B, IV)
+    b = bytearray()
+    [b.extend(PUT_UINT32_BE(each)) for each in y]
+    return bytes(b)
+
+
+def hash_msg(msg):
+    return digest(msg, 0).hex()
+
+
+def str2bytes(msg: str, encoding='utf-8'):
+    """
+    字符串转换成byte数组
+    :param msg: 信息串
+    :param encoding: 编码
+    :return:
+    """
+    msg_bytearray = msg.encode(encoding) if isinstance(msg, str) else msg
+    return list(msg_bytearray)
+
+
+def byte2str(msg, decode='utf-8'):
+    """
+    byte数组转字符串
+    :param msg:
+    :param decode:
+    :return:
+    """
+    return msg.decode(decode) if isinstance(msg, (bytes, bytearray)) else msg
+
+
+def hex2byte(msg):
+    """
+    16进制字符串转换成byte列表
+    :param msg:
+    :return:
+    """
+    if not isinstance(msg, str):
+        raise ValueError('message must be string')
+    ml = len(msg)
+    if (ml & 1) != 0:
+        msg = '0' + msg
+    return list(bytes.fromhex(msg))
+
+
+def Hash_sm3(msg, Hexstr=0):
+    msg_byte = hex2byte(msg) if Hexstr else str2bytes(msg)
+    return hash_msg(msg_byte)
+
+
+hexdigest = Hash_sm3
+
+
+def _BKDF(Z, klen: int):
+    """
+    :param Z: Z为16进制表示的比特串（str），
+    :param klen: klen为密钥长度（单位byte）
+    :return:
+    """
+    klen = int(klen)
+    rcnt = int(ceil(klen / 32))
+    Zin = hex2byte(Z)
+    b = bytearray()
+    [b.extend(digest(Zin + PUT_UINT32_BE(ct), 0)) for ct in range(1, rcnt + 1)]
+    return b[:klen]
+
+
+def KDF(Z, klen: int):
+    """
+    :param Z: Z为16进制表示的比特串（str），
+    :param klen: klen为密钥长度（单位byte）
+    :return:
+    """
+    return _BKDF(Z, klen).hex()
+
+
+class SM3Type(object):
+    name = 'SM3'
+    digest_size = 32
+    block_size = 64
+
+    def __init__(self, msg=b'', encoding='utf-8'):
+        self.encoding = encoding
+        self.msg = bytearray(str2bytes(msg, self.encoding))
+
+    def update(self, msg):
+        self.msg.extend(str2bytes(msg, self.encoding))
+
+    def digest(self):
+        return digest(self.msg, 0)
+
+    def hexdigest(self):
+        return hexdigest(self.msg, 0)
+
+    def copy(self):
+        return copy(self)
+
+
+SM3 = SM3Type
+
+del CF2
+del CF3
